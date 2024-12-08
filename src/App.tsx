281adaec199a407c7ba41c11d10c88db66371240
urke90 <urosbijelic90@gmail.com>
2024-12-08 23:14:25 +0100

    remove comments from Button.tsx component and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import Button from './components/ui/Button';
-
 function App() {
   return (
     <div>
       <h1 className="h1-regular">Konfigurator servisa</h1>
-      <div
-        style={{
-          margin: 'auto',
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <Button className="btn--secondary btn--small">Text</Button>
-      </div>
-      {/* <br />
-      <br />
-      <Button className="btn btn--tertiary">This is tertiary button</Button> */}
     </div>
   );
 }
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,6 @@
 import { generateClassName, removeWhitespaceFromClassName } from '@/lib/utils';
-
 import type { ComponentPropsWithoutRef } from 'react';
+
 import classes from './Button.module.scss';
 
 // ----------------------------------------------------------------
@@ -8,12 +8,7 @@ import classes from './Button.module.scss';
 interface IButtonProps extends ComponentPropsWithoutRef<'button'> {}
 
 const Button: React.FC<IButtonProps> = ({ className = '', ...rest }) => {
-  // console.log('classes', classes);
-  // console.log('className', className);
-  // console.log('className trim()', className.trim());
-
   const transformedClassNames = removeWhitespaceFromClassName(className);
-
   const createdClases = generateClassName(classes, transformedClassNames);
 
   return <button className={`${classes.btn} ${createdClases}`} {...rest} />;
