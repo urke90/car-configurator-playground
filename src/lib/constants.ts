b91aeba2762642980d7b0124e6463c6065287f06
urke90 <urosbijelic90@gmail.com>
2024-12-11 13:49:48 +0100

    create html structure for choosing car model section with radio buttoons

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -6,3 +6,31 @@ export enum EWizzardPage {
   OVERVIEW_PAGE = 'OVERVIEW_PAGE',
   COMPLETION_PAGE = 'COMPLETION_PAGE',
 }
+
+export const CAR_MODELS = [
+  {
+    id: '1',
+    label: 'Peugeot',
+    value: 'peugeot',
+  },
+  {
+    id: '2',
+    label: 'Volkswagen',
+    value: 'olkswagen',
+  },
+  {
+    id: '3',
+    label: 'Citroen',
+    value: 'citroen',
+  },
+  {
+    id: '4',
+    label: 'BMW',
+    value: 'bmw',
+  },
+  {
+    id: '5',
+    label: 'Seat',
+    value: 'seat',
+  },
+];
--- src/pages/ServicePage.tsx
+++ src/pages/ServicePage.tsx
@@ -1,3 +1,5 @@
+import RadioButton from '@/components/ui/RadioButton';
+import { CAR_MODELS } from '@/lib/constants';
 import classes from './ServicePage.module.scss';
 
 // ----------------------------------------------------------------
@@ -8,6 +10,16 @@ const ServicePage: React.FC<IServicePageProps> = (props) => {
   return (
     <section className={classes.service}>
       <h2 className={classes.service__title}>Konfigurator Servisa</h2>
+      <div className={`${classes.service__section}`}>
+        <h4 className={classes['service__section-title']}>Odaberite proizvođača vašeg vozila</h4>
+        <div className={classes['service__car-models']}>
+          {CAR_MODELS.map(({ id, label, value }) => (
+            <div key={id} style={{ minWidth: '166px' }}>
+              <RadioButton label={label} value={value} />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
