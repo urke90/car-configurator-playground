e7f7b5177b76d8e7d323d37b40d72070347b2b86
urke90 <urosbijelic90@gmail.com>
2024-12-13 10:31:27 +0100

    finish services details section

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -1,19 +1,31 @@
+import { formatPrice } from '@/lib/utils';
 import { useFormContext } from 'react-hook-form';
 
 import classes from './Overview.module.scss';
 
 // ----------------------------------------------------------------
 
-const carModelStyles = {
-  display: 'flex',
-  gap: '5px',
-  paddingTop: '20px',
-};
-
 const OverviewPage = () => {
   const { getValues } = useFormContext();
 
-  const data = getValues();
+  const data = {
+    carModel: 'Peugeot',
+    services: ['Zamjena ulja i filtera', 'Servis klima uređaja'],
+    price: 105.0,
+    totalDiscount: 21.0,
+    discount: {
+      id: '1',
+      code: 'Kupon123',
+      amount: 20,
+      type: 'percentage',
+    },
+    user: {
+      name: 'John Doe',
+      phone: '091 123 4567',
+      email: 'john.doe@mail.com',
+      note: 'Kod auta se prilikom prelaska preko rupa čuje lupanje pa bi i to trebalo pogledati',
+    },
+  };
 
   console.log('data U OVERVIEW PAGE-U', data);
 
@@ -30,10 +42,35 @@ const OverviewPage = () => {
       </div>
       <div className={classes.overview__section}>
         <div className={classes['overview__details']}>
-          <div style={carModelStyles}>
+          <div className={classes['overview__details-section']} style={{ paddingTop: '20px' }}>
             <h4 className={classes['overview__details-title']}>Model vozila</h4>
             <h5 className={classes['overview__details-text']}>{data.carModel}</h5>
           </div>
+          <div className={classes['overview__details-section']} style={{ paddingTop: '15px' }}>
+            <h4 className={classes['overview__details-title']}>Odabrane usluge</h4>
+            <div className={classes['overview__details-row']}>
+              <h5 className={classes['overview__details-text']}>Zamjena ulja i filtera</h5>
+              <h5 className={classes['overview__details-text']}>65,00 €</h5>
+            </div>
+            {data.discount.id && (
+              <div className={classes['overview__details-price-wrapper']}>
+                <h5 className={classes['overview__details-text']}>
+                  Popust {data.discount.amount}
+                  {data.discount.type === 'percentage' ? '%' : '€'}{' '}
+                </h5>
+                <h5 className={classes['overview__details-text']}>
+                  -{formatPrice(data.totalDiscount)}
+                </h5>
+              </div>
+            )}
+            <div className={classes['overview__details-price-wrapper']}>
+              <h5 className={classes['overview__details-text']}>Ukupno:</h5>
+              <h5 className={classes['overview__details-text']}>{formatPrice(data.price)}</h5>
+            </div>
+          </div>
+          {/* <div className={classes['overview__details-section']} style={{ paddingTop: '15px', paddingBottom: '20px' }}>
+            <h4 className={classes['overview__details-title']}>Kontakt podaci</h4>
+          </div> */}
         </div>
       </div>
     </section>
